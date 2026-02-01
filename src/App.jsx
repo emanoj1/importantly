88431f548fd8e73927c07a558e0fe65f1f3261f1
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2026-02-01 15:16:09 +1100

    Rename to "Home.jsx"
    To force change Git

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import Home from "./pages/Home_temp";
+import Home from "./pages/Home";
 import TrueProfitPrivacy from "./pages/privacy/shopifyapps/TrueProfitPrivacy";
 
 export default function App() {
